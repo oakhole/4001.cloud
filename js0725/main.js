79d51564e1eb3d7360ff7a7f3447a764bafc87bd
@qiuyang4916 <17755525734@139.com>
2020-08-27 18:46:46 +0800

    设置滚动宽度

--- js0725/main.js
+++ js0725/main.js
@@ -61,14 +61,14 @@ function init_case(pageSize, pageNum) {
 		type: "POST",
 		success: function(e) {
 			var total = e.total, //读取到的数据数量
-				html = "<ul class='ss'>";
+				html = "<div class='ss'>";
 			for (var i = 0; i < total; i++) {
-				html += "<li><a target='_blank' href='" + base_url + "/cms/post/" + e.rows[i].postId +
+				html += "<div><a target='_blank' href='" + base_url + "/cms/post/" + e.rows[i].postId +
 					".html'><div class='item-img'><img src=" + e.rows[i].coverImg +
 					"></img></div><div class='case_span'><span>" + JSON.parse(e.rows[i].metaInfo).calleeNumber +
-					"</span></div><div class='case_hide'><p>点击查看案例详情</p></div></li></a>"
+					"</span></div><div class='case_hide'><p>点击查看案例详情</p></div></div></a>"
 			}
-			html += "</ul>";
+			html += "</div>";
 			document.querySelector(".caselist .cc").innerHTML = html + html;
 			var height = document.querySelector(".caselist .ss").offsetWidth;
 			addKeyFrames('-' + height + 'px'); // 设置keyframes
