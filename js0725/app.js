df448bd9d379a27662583aec62f1d411ffa35963
qiuyang <17755525734@139.com>
2021-08-06 15:19:16 +0800

    提交

--- js0725/app.js
+++ js0725/app.js
@@ -97,10 +97,10 @@ function showCaleeNumberList(pattern) {
 				});
 			}
 			console.log(newData);
-			if(newData.length == 0){
+			if (newData.length == 0) {
 				$(".spinner").hide();
 				$(".spinner2").show();
-			}else{
+			} else {
 				$(".spinner").hide();
 				$(".spinner2").hide();
 				$("#number-list").append(numberListTemplate(newData));
@@ -133,22 +133,12 @@ let longPollingLogin = function() {
 	});
 };
 
-<<<<<<< HEAD
 let refreshOccupiedNumberTable = function() {
 	// 刷新已预占号码数据
-	$.get("http://localhost:8000/tenant/bookRepo/occupied?calleeNumber=&companyName=" + wxUser.openId, function(
-	res) {
+	$.get("https://400cha.cn/tenant/bookRepo/occupied?calleeNumber=&companyName=" + wxUser.openId, function(res) {
 		$("#tableOccupied").bootstrapTable("load", res.data);
 		$("#occupiedNumberModal").modal();
 	});
-=======
-let refreshOccupiedNumberTable = function () {
-  // 刷新已预占号码数据
-  $.get("https://400cha.cn/tenant/bookRepo/occupied?calleeNumber=&companyName=" + wxUser.openId, function (res) {
-    $("#tableOccupied").bootstrapTable("load", res.data);
-    $("#occupiedNumberModal").modal();
-  });
->>>>>>> d3a3b2adc0ba0fd7c8c28b731a724a1ea3402b20
 };
 
 /**
